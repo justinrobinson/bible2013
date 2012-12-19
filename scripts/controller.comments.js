a55b85c9a400f249ef8564d5b408d6995e4d5b78
adamexmachina <adam.macchi@gmail.com>
2012-12-19 12:34:34 -0500

    Adding login. Styling login/register to respond to form height.

--- scripts/controller.comments.js
+++ scripts/controller.comments.js
@@ -4,6 +4,30 @@ bible2012App.controller('CommentsCtrl', function($http, $scope, $rootScope, $log
     $scope.pageNumber = 1;
     $scope.commentCount = 100;
     $scope.commentsAvailable = false;
+    $scope.textarea = 'Add your comment.';
+    $scope.submitComment = function() {
+        $log.log('New comment is:', $scope.textarea);
+    };
+    /*
+     *
+     * Object {article: Object}
+article: Object
+comment_count: "42"
+comments: Array[10]
+0: Object
+$$hashKey: "00Y"
+can_delete: "No"
+comment_id: "2866541"
+comment_rating: "0"
+comment_text: "&quot;Father my loving King in Jesus name I praise you! Glory to You Lord Jesus Christ forever! Holy Spirit be blessed! Lord Jehovah in Jesus name I praise You for Your Holy Word stands forever! Your promises are yes and amen!! Thank You Father for Your love in Jesus name! Thank You my Father for the favor of God is upon me in Jesus name! Father Your Word says in John 14:14,'Ask Anything In My Name And I will Do It!&quot;, I believe You Father and I expect to receive based on Your truth and faithfulness!! Glory Hallelujah Jesus!! Psalm 37:4,'delight thyself in the Lord and He shall give you the desires of your heart!'. I believe and I receive in Jesus name I thank You Father Jehovah Yahweh, Amen!!&quot;"
+display_name: "Peter Frank Santovito"
+friendly_time: "2 hours ago"
+mycbn_user_name: "4fd97bb2-caa5-4e7a-8b26-74a21b2f09c0"
+profile_img_src: "http://my.cbn.com/mod/profile/icondirect.php/?username=4fd97bb2-caa5-4e7a-8b26-74a21b2f09c0&size=small"
+profile_link: "http://my.cbn.com/pg/profile/4fd97bb2-caa5-4e7a-8b26-74a21b2f09c0"
+unix_timestamp: "1355917426"
+
+*/
     $scope.previous = function() {
         if ($scope.pageNumber != 1) {
             $scope.nextEnabled = true;
